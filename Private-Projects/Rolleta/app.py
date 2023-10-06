371ec1e665d38f65f62779c7957fd7d1bb0dc337
YBO <yitzhak851@gmail.com>
2023-10-06 10:01:55 +0300

    adding rolleta file

---
+++ Private-Projects/Rolleta/app.py
@@ -0,0 +1,19 @@
+import this
+import random
+
+print("Welcome to cala game Rolleta")
+START_VALUE = input("Enter the first coin you start(at integer number): ")
+HOU_MANY_STEPS = input("Enter the number of steps")
+
+Using random.shuffle() function
+
+
+result = input("Enter three digits (each digit for one pig): ")
+
+
+hundreds = result // 100
+print(hundreds)
+tens = result % 100
+print(tens)
+digit = result % 10
+print(digit)
